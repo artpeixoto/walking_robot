86cb76c3667e84892e30bec3ea28333577476dad
artur.peixoto <arturokfya@gmail.com>
2025-03-07 12:13:39 -0300

    enhanced training loop

--- walking_robot_brain/src/main.rs
+++ walking_robot_brain/src/main.rs
@@ -1,12 +1,12 @@
 use std::{
-    collections::VecDeque, ops::Not, path::{Path, PathBuf}, str::FromStr, time::Duration
+    collections::VecDeque, iter, ops::Not, path::{Path, PathBuf}, str::FromStr, time::Duration
 };
 
 use burn::{
     backend::{wgpu::WgpuDevice, Autodiff, Wgpu}, config::Config, module::Module, nn::loss::MseLoss, optim::{AdaGrad, AdaGradConfig, Adam, AdamConfig, AdamW, AdamWConfig, GradientsParams, Optimizer, SgdConfig}, prelude::Backend, record::{DefaultFileRecorder, FileRecorder, FullPrecisionSettings}, tensor::{backend::AutodiffBackend, Distribution::Uniform, Tensor}};
 use comm::{SimulationConnector, SimulationEndpoint};
 use models::{a_selector::ASelectorConfig, rs_estimator::{RsEstimator, RsEstimatorConfig}, v_estimator::{VEstimator, VEstimatorConfig}};
-use rand::{rngs::ThreadRng, Rng};
+use rand::{rngs::ThreadRng, seq::IndexedRandom, Rng};
 use tensor_conversion::TensorConvertible;
 use tokio::{main, time::timeout};
 use tools::UsedInTrait;
@@ -117,14 +117,14 @@ async fn async_main(){
         info!("No models were found. A first batch of learning from random actions will start"); 
 
         let mut histories = Vec::new();
-        for _i in 0..100{
+        for _i in 0..20{
             let policy = |_state| {
                 GameAction::random(&mut rng)
             };
             histories.push(simulation.run_episode(policy).await);
         }
         info!("starting learning batch..."); 
-        for (_ix, history) in histories.iter().enumerate(){
+        for history in iter::from_fn(||histories.choose(&mut rng)).take(1000){
             debug!("training rs_estimator ");
             rs_estimator = rs_estimator.train(&history, rs_est_lr, &mut rs_est_opt, &mut MseLoss::new(), &dev);           
         }            
@@ -133,39 +133,39 @@ async fn async_main(){
         v_estimator.clone().save_file(v_estimator_model_path.clone(), &recorder).unwrap();
         a_selector.clone().save_file(a_selector_model_path.clone(), &recorder).unwrap();
 
-        const INITIAL_TRAINING_LOOP_COUNT:usize = 100;
-        info!("Starting training");
-        for initial_training_loop in 0..INITIAL_TRAINING_LOOP_COUNT{
-            info!("Collecting episodes...");
-            let mut histories = Vec::new();
-            for _ in 0..10{
-                let policy = |state| {
-                    if rng.random::<f32>() < ( initial_training_loop as f32 / INITIAL_TRAINING_LOOP_COUNT as f32){
-                        a_selector.select_action(&state, &dev)
-                    } else {
-                        GameAction::random(&mut rng)
-                    }
-                };
-                let history = simulation.run_episode(policy).await;
+        // const INITIAL_TRAINING_LOOP_COUNT:usize = 100;
+        // info!("Starting training");
+        // for initial_training_loop in 0..INITIAL_TRAINING_LOOP_COUNT{
+        //     info!("Collecting episodes...");
+        //     let mut histories = Vec::new();
+        //     for _ in 0..10{
+        //         let policy = |state| {
+        //             if rng.random::<f32>() < ( initial_training_loop as f32 / INITIAL_TRAINING_LOOP_COUNT as f32){
+        //                 a_selector.select_action(&state, &dev)
+        //             } else {
+        //                 GameAction::random(&mut rng)
+        //             }
+        //         };
+        //         let history = simulation.run_episode(policy).await;
 
-                histories.push(history);     
-            }
+        //         histories.push(history);     
+        //     }
 
-            for history in histories.into_iter(){
-                info!("training rs_estimator ");
-                rs_estimator = rs_estimator.train(&history, rs_est_lr, &mut rs_est_opt, &mut MseLoss::new(), &dev);
+        //     for history in histories.into_iter(){
+        //         info!("training rs_estimator ");
+        //         rs_estimator = rs_estimator.train(&history, rs_est_lr, &mut rs_est_opt, &mut MseLoss::new(), &dev);
 
-                info!("training v_estimator with monte carlo");
-                v_estimator = v_estimator.monte_carlo_train(&history, alpha, v_est_mc_lr, &mut v_est_mc_opt, &mut MseLoss::new(), &dev);
+        //         info!("training v_estimator with monte carlo");
+        //         v_estimator = v_estimator.monte_carlo_train(&history, alpha, v_est_mc_lr, &mut v_est_mc_opt, &mut MseLoss::new(), &dev);
 
-                info!("training v_estimator with td");
-                v_estimator = v_estimator.td_train(&history, &rs_estimator, &a_selector, alpha, 20, 10, 5, v_est_td_lr, &mut v_est_td_opt, &mut MseLoss::new(), &dev);
+        //         info!("training v_estimator with td");
+        //         v_estimator = v_estimator.td_train(&history, &rs_estimator, &a_selector, alpha, 20, 10, 5, v_est_td_lr, &mut v_est_td_opt, &mut MseLoss::new(), &dev);
 
-                info!("training a_selector");
-                a_selector = a_selector.train(&history, &rs_estimator, 100, &v_estimator, &mut a_sel_opt, &mut MseLoss::new(), alpha, a_sel_lr, &dev);
-            }
-        } 
-    }
+        //         info!("training a_selector");
+        //         a_selector = a_selector.train(&history, &rs_estimator, 100, &v_estimator, &mut a_sel_opt, &mut MseLoss::new(), alpha, a_sel_lr, &dev);
+        //     }
+        // } 
+    } 
 
     '_MAIN_TRAIN_LOOP: loop {          
         let mut histories = Vec::new();
